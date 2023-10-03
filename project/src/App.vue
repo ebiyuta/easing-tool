f9941843b1dee52fb215968b985628fc5c8c8f00
ebiyuta <xxpino123pinoxx@gmail.com>
2023-10-04 02:17:35 +0900

    不要なコンポーネントの削除とざっくりとした、大枠の作成

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,47 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import EasingTool from './components/EasingTool.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <EasingTool />
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.main {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ project/src/components/EasingTool.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="easingTool">
+    <div class="easingTool_preview">TODO：上部のアニメーションプレビュー部分</div>
+    <div class="easingTool_preset">TODO：左側プリセット一覧部分</div>
+    <div class="easingTool_body">TODO：イージングツールの本体部分</div>
+    <div class="easingTool_slider">TODO：下部のスライダー部分</div>
+  </div>
+</template>
+
+<style scoped>
+.easingTool {
+  display: grid;
+  grid-template:
+    'preview preview' 30px
+    'preset body' 1fr
+    'slider slider' 30px / 80px 1fr;
+  width: 260px;
+  height: 350px;
+}
+
+.easingTool_preview {
+  grid-area: preview;
+  background-color: red;
+}
+
+.easingTool_preset {
+  grid-area: preset;
+  background-color: green;
+}
+
+.easingTool_body {
+  grid-area: body;
+  background-color: blue;
+}
+
+.easingTool_slider {
+  grid-area: slider;
+  background-color: yellow;
+}
+</style>
