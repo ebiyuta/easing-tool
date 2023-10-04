401ed4e8af37801d85b81e0ddd298c0623d5339a
ebiyuta <xxpino123pinoxx@gmail.com>
2023-10-05 00:10:03 +0900

    EasingToolBody：見た目に必要な要素をsvgで記述

--- project/src/App.vue
+++ project/src/App.vue
@@ -5,9 +5,6 @@ import EasingTool from './components/EasingTool.vue'
 <template>
   <main class="main">
     <EasingTool />
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
   </main>
 </template>
 
--- project/src/components/EasingTool.vue
+++ project/src/components/EasingTool.vue
@@ -1,26 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EasingToolBody from "@/components/EasingToolBody/EasingToolBody.vue";
+</script>
 
 <template>
-  <div class="easingTool">
-    <div class="easingTool_preview">TODO：上部のアニメーションプレビュー部分</div>
-    <div class="easingTool_preset">TODO：左側プリセット一覧部分</div>
-    <div class="easingTool_body">TODO：イージングツールの本体部分</div>
-    <div class="easingTool_slider">TODO：下部のスライダー部分</div>
+  <div class="EasingTool">
+    <div class="EasingTool_preview">TODO：上部のアニメーション brプレビュー部分</div>
+    <div class="EasingTool_preset">TODO：左側プリセット一覧部分</div>
+    <div class="EasingTool_body">
+      <EasingToolBody/>
+    </div>
+    <div class="EasingTool_slider">TODO：下部のスライダー部分</div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.easingTool {
+.EasingTool {
   display: grid;
   grid-template:
-    'preview preview' 30px
+    'preview preview' 54px
     'preset body' 1fr
-    'slider slider' 30px / 80px 1fr;
-  width: 260px;
+    'slider slider' 47px / 66px 1fr;
+  width: 270px;
   height: 350px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
 
   &_preview {
     grid-area: preview;
+    word-break: break-all;
     background-color: red;
   }
 
@@ -31,7 +37,6 @@
 
   &_body {
     grid-area: body;
-    background-color: blue;
   }
 
   &_slider {
