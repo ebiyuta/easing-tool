3702c2b47e476fbe1bf78997573015275f1c122e
ebiyuta <xxpino123pinoxx@gmail.com>
2023-10-06 00:27:37 +0900

    EasingToolBody：マウスがSVG上からはみ出しても正常にハンドル操作を行えるように

--- project/src/components/EasingToolBody/EasingToolBody.vue
+++ project/src/components/EasingToolBody/EasingToolBody.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { onMounted, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref } from 'vue'
 
 // 始点の座標
 const START_POINT = {
-  x: 2,
+  x: 10,
   y: 185
 }
 // 終点の座標
@@ -22,26 +22,72 @@ const endHandle = {
   y: 50
 }
 
-const wrapperRef = ref(null)
+const wrapperRef = ref<SVGElement | null>(null)
 
 onMounted(() => {
-  // TODO：以下、簡易的な動作確認用コードなので削除予定
   // svgタグのDOM取得
-  const wrapperDom: HTMLElement | null = wrapperRef.value
+  const wrapperDom = wrapperRef.value
 
-  // 早期リターン
   if (!wrapperDom) {
+    // 型ガード
     return
   }
-  const test = (e) => {
+
+  // クリック開始座標
+  let clickStartPoint = {
+    x: 0,
+    y: 0
+  }
+
+  // クリック開始時段階の始点ハンドルの座標
+  let currentHandle = {
+    x: 0,
+    y: 0
+  }
+
+  const moveHandle = (e: MouseEvent) => {
+    /**
+     * ハンドルのX座標を計算します。（現在のハンドル座標 - 移動距離）
+     * 移動距離の計算は、クリック開始地点 - 現在のマウス座標。
+     * X座標のみ左右の移動制限があります。
+     */
+    const x = computed(() => {
+      if (currentHandle.x - (clickStartPoint.x - e.clientX) < START_POINT.x) {
+        return START_POINT.x
+      } else if (currentHandle.x - (clickStartPoint.x - e.clientX) > END_POINT.x) {
+        return END_POINT.x
+      } else {
+        return currentHandle.x - (clickStartPoint.x - e.clientX)
+      }
+    })
+
+    /**
+     * ハンドルのY座標を計算します。（現在のハンドル座標 - 移動距離）
+     */
+    const y = computed(() => currentHandle.y - (clickStartPoint.y - e.clientY))
+
+    // ハンドルに計算結果を反映
+    startHandle.x = x.value
+    startHandle.y = y.value
+  }
+  wrapperDom.onmousedown = (e: MouseEvent) => {
+    // TODO: ハンドルとクリック箇所の座標を比較し、より近いハンドルを握るように
+    // クリック開始座標をブラウザ全体を起点に取得
+    clickStartPoint.x = e.clientX
+    clickStartPoint.y = e.clientY
+    // クリック開始座標をSVGタグを起点に取得
     startHandle.x = e.offsetX
     startHandle.y = e.offsetY
-  }
-  wrapperDom.onmousedown = () => {
-    document.addEventListener('mousemove', test)
+    // SVGタグ起点のクリック開始座標をハンドルに反映
+    currentHandle.x = startHandle.x
+    currentHandle.y = startHandle.y
 
-    wrapperDom.onmouseup = () => {
-      document.removeEventListener('mousemove', test)
+    // ドラッグ中に行う処理
+    document.addEventListener('mousemove', moveHandle)
+
+    // ドラッグ終了時に行う処理
+    document.onmouseup = () => {
+      document.removeEventListener('mousemove', moveHandle)
     }
   }
 })
