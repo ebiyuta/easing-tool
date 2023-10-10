d8fa948817b99cd36b9390551cf1cc592084d474
ebiyuta <xxpino123pinoxx@gmail.com>
2023-10-10 23:39:23 +0900

    build：ビルド設定の調整

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-      outDir: '../docs'
+    outDir: '../docs',
+    assetsDir: 'easing-tool',
   },
   plugins: [
     vue(),
