53619554cbebb1bbe09c331a1a65b2a7233c6ef9
ebiyuta <xxpino123pinoxx@gmail.com>
2023-10-11 00:36:28 +0900

    build：configの調整

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    base: "easing-tool",
-  },
+  base: "easing-tool",
   plugins: [
     vue(),
   ],
